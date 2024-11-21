bc1fcf2dbc64719d9a21e4fd06416fae634eeec6
Gabriel Eduardo Farinha <94718937+GabrielEduardoFarinha@users.noreply.github.com>
2024-11-21 16:52:07 -0300

    Commit conexao com DB
    Commit da aula 3 com conexao com o banco de dados MONGO

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
 const posts = [
     { id: 1, description: "test photos", images: "https://placecats.com/millie/300/150" },
-    { id: 2,description: "A majestic sunset", images: "https://example.com/sunset1.jpg" },
-    { id: 3,description: "A majestic over", images: "https://example.com/sunset1.jpg" },
+    { id: 2,description: "A majestic sunset", images: "https://placecats.com/millie_neo/300/200" },
+    { id: 3,description: "A majestic over", images: "https://placecats.com/millie/300/150" },
   ];
 
 
@@ -14,20 +16,27 @@ app.listen(3000, () => {
     console.log("im listing...");
 });
 
-app.get("/posts", (req, res) => {
+async function getAllPosts(){
+    const db = conexao.db("instabytes")
+    const colecao = db.collection("posts")
+    return colecao.find().toArray()
+}
+
+app.get("/posts", async (req, res) => {
+    const posts = await getAllPosts()
     res.status(200).json(posts);
 });
 
-function searchPostsID(id) {
-    return posts.findIndex((post) => {
-        return post.id ===Number(id)
-
-    })
-}
-
-
-
-app.get("/posts/:id", (req, res) => {
-    const index = searchPostsID(req.params.id)
-    res.status(200).json(posts[index]);
-});
+//function searchPostsID(id) {
+//    return posts.findIndex((post) => {
+//        return post.id ===Number(id);
+//
+//    })
+//};
+//
+//
+//
+//app.get("/posts/:id", (req, res) => {
+//    const index = searchPostsID(req.params.id)
+//    res.status(200).json(posts[index]);
+//});
